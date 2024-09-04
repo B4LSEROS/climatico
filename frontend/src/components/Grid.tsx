6c03026509b20c6f785d3a958b542d740aefd591
Juani <juanarielok@gmail.com>
2024-09-04 19:02:56 -0300

    mi rey

--- frontend/src/components/Grid.tsx
+++ frontend/src/components/Grid.tsx
@@ -1,7 +1,7 @@
 export default function Grid() {
   return (
     <section role="grid" className="flex min-h-screen flex-col">
-      <img src="/images/Sun2.png" alt="Sun image" width={100} height={24} />
+      <img className="sunnygrid" src="/images/Sun2.png" alt="Sun image" width={100} height={24} />
     </section>
   );
 }
