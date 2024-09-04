ff2f00df393cb0330f05d7e23bad7ce7e9bcd52f
Jose <davidbalserosp@gmail.com>
2024-09-04 11:34:19 -0400

    🏗️ DevOps: CI pipeline setup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
+import SideBar from "@/components/SideBar";
+import Sun2 from "../../public/images/Sun2.png";
+import CloudwithRain from "../../public/images/CloudwithRain.png";
+import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center align-items p-24">
+      <Grid />
+      <Image src={Sun2} alt="Sun image" width={50} height={24} />
+      <Image
+        src={CloudwithRain}
+        alt="Cloud with rain image"
+        width={100}
+        height={24}
+      />
+      <SideBar title="Climatico" />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
