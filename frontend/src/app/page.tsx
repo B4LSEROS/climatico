5bde4ee0c4e3693ac085aa64cb41316fa9b8f1b2
Jose <davidbalserosp@gmail.com>
2024-09-13 18:44:41 -0400

    📝 Add: API endpoint refinement for weather conversion and type assertion.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import personUmbrella from "../../public/utilities/personUmbrella.png";
-import DescriptionContainer from "@/components/weather/descriptionContainer";
 import MyLocationContainer from "@/components/weather/myLocationContainer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center align-items p-24">
-      <DescriptionContainer />
       <hr />
       <Image
         src={personUmbrella}
